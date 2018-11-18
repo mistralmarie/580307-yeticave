f0fc36fb8f78382de90617282aa9a3807a9f3709
Мария <tropaapachi@gmail.com>
2018-11-18 21:36:07 +0200

    работа с условиями

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $is_auth = rand(0, 1);
 
-$user_name = ''; // укажите здесь ваше имя
+$user_name = "Marie"; // укажите здесь ваше имя
 $user_avatar = 'img/user.jpg';
 ?>
 <!DOCTYPE html>
@@ -15,23 +15,50 @@ $user_avatar = 'img/user.jpg';
 <body>
 <div class="page-wrapper">
 
+
+
 <header class="main-header">
-    <div class="main-header__container container">
-        <h1 class="visually-hidden">YetiCave</h1>
-        <a class="main-header__logo">
-            <img src="img/logo.svg" width="160" height="39" alt="Логотип компании YetiCave">
-        </a>
+ <div class="main-header__container container">
+  <h1 class="visually-hidden">YetiCave</h1>
+   <a class="main-header__logo" href="/">
+    <img src="img/logo.svg" width="160" height="39" alt="Логотип компании YetiCave">
+   </a>
         <form class="main-header__search" method="get" action="https://echo.htmlacademy.ru">
             <input type="search" name="search" placeholder="Поиск лота">
             <input class="main-header__search-btn" type="submit" name="find" value="Найти">
         </form>
-        <a class="main-header__add-lot button" href="pages/add-lot.html">Добавить лот</a>
+    <a class="main-header__add-lot button" href="pages/add-lot.html">Добавить лот</a>
+     <nav class="user-menu" >
+	 <?php if ($is_auth == 1): ?>
+	 
 
-        <nav class="user-menu">
-
-        <!-- здесь должен быть PHP код для показа аватара пользователя -->
-
-        </nav>
+        
+  <div class="user-menu__image">
+  <img src="img/user.jpg" width="40" height="40" alt="Пользователь">
+  </div>
+  <div class="user-menu__logged">
+   <p>
+  <?= $user_name;?>
+   </p>
+   <a href="logout.php">Выйти</a>
+   </div>
+   <?php else :?>
+   
+   
+	<ul class="user-menu__list">
+    <li class="user-menu__item">
+      <a href="#">Регистрация</a>
+    </li>
+    <li class="user-menu__item">
+      <a href="#">Вход</a>
+    </li>
+  </ul>
+  
+  <?php endif ; ?>
+  
+        </nav> 
+		
+	
     </div>
 </header>
 
